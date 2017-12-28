075a686ab23a1f64b678a053f714a838b58ee10a
San Guyer <sanguyer@gmail.com>
2017-12-27 20:09:04 -0500

    Views and hbs setup

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const app = express();
+const exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 module.exports = app;
